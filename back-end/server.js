7a79b19341cfdb0671f925398ba41dc8c238be3e
Kyle Nielsen <kwnielsen17@gmail.com>
2021-12-06 17:52:49 -0700

    Part 5 finished

--- back-end/server.js
+++ back-end/server.js
@@ -32,7 +32,9 @@ app.use(cookieSession({
 }));
 
 // import the users module and setup its API path
-const users = require("./users.js");
-app.use("/api/users", users.routes);
+const users = require('./users.js');
+app.use('/api/users', users.routes);
+const photos = require('./photos.js');
+app.use('/api/photos', photos.routes);
 
 app.listen(3001, () => console.log('Server listening on port 3001!'));
